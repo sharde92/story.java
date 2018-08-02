4bbe9a1942ac8c6e868e80e859814bd62bba3085
sharde <sharde.m.smith@gmail.com>
2018-08-02 10:21:40 -0500

    addline

--- src/story.java
+++ src/story.java
@@ -1,4 +1,10 @@
 
 public class story {
 
+	public static void main(String [] args) {
+		
+	System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys");
+	System.out.print("They were the most respected band of ninja monkeys");
+	
+	}
 }
